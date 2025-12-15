ea4844ca1415c00516b620966ebb57154e7f0eac
MC-GJosh <joshcastillo0231@gmail.com>
2025-12-16 01:17:42 +0800

    Allow all CORS origins for debugging

--- Resort-api/config/cors.php
+++ Resort-api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3002', 'http://127.0.0.1:3002', env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
